d03b94d070899868f4c0e32fefa801c88fb8a339
collins <collinsleonkimani@outlook.com>
2023-04-24 10:00:53 +0300

    try to run transactions

--- transaction-palace/src/App.js
+++ transaction-palace/src/App.js
@@ -22,4 +22,58 @@ function App() {
   );
 }
 
+// export default App;
+
+// import React, { useState, useEffect } from "react";
+// import TransactionsTable from "./components/TransactionsTable";
+// import AddTransactionForm from "./components/AddTransactionForm";
+// import SearchBar from "./components/SearchBar";
+
+// function App() {
+//   const [transactions, setTransactions] = useState([]);
+
+//   useEffect(() => {
+//     // fetch data from local server running JSON DB server
+//     // and store it in state
+//   }, []);
+
+//   const handleAddTransaction = (newTransaction) => {
+//     // add new transaction to state
+//   };
+
+//   const handleDeleteTransaction = (transactionId) => {
+//     // remove transaction from state
+//   };
+
+//   const handleSearchTransactions = (searchTerm) => {
+//     // filter transactions by search term
+//   };
+
+//   const handleSortTransactions = (sortBy) => {
+//     // sort transactions by category or description
+//   };
+
+//   return (
+//     <div className="App">
+//       <header>
+//         <h1>Bank Transactions</h1>
+//         <img src="/logo.png" alt="Logo" />
+//       </header>
+//       <main>
+//         <TransactionsTable
+//           transactions={transactions}
+//           onDeleteTransaction={handleDeleteTransaction}
+//           onSortTransactions={handleSortTransactions}
+//         />
+//         <AddTransactionForm onAddTransaction={handleAddTransaction} />
+//       </main>
+//       <aside>
+//         <SearchBar onSearchTransactions={handleSearchTransactions} />
+//       </aside>
+//     </div>
+//   );
+// }
+
 export default App;
+
+
